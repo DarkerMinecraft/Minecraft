56e1c64d1ff138300c1bde7e1cd27edd41cb20bb
Ben Nelson <nayongbin8@gmail.com>
2021-02-04 11:55:39 -0500

    Init Commits

---
+++ premake5.lua
@@ -0,0 +1,63 @@
+workspace "Minecraft"
+	architecture "x64"
+	startproject "Minecraft"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+IncludeDir = {}
+IncludeDir["GLFW"] = "vendor/GLFW/include"
+IncludeDir["Glad"] = "vendor/Glad/include"
+IncludeDir["glm"] = "vendor/glm"
+
+group "Dependencies"
+	include "vendor/GLFW"
+	include "vendor/Glad"
+group ""
+
+project "Minecraft"
+	location "Minecraft"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/Source/**.h",
+		"%{prj.name}/Source/**.cpp"
+	}
+
+	includedirs 
+	{
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.Glad}",
+		"%{IncludeDir.glm}"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		filter "configurations:Debug"
+			defines "EN_DEBUG"
+			runtime "Debug"
+			symbols "on"
+			
+		filter "configurations:Release"
+			defines "EN_RELEASE"
+			runtime "Release"
+			optimize "on"
+
+		filter "configurations:Dist"
+			defines "EN_DIST"
+			runtime "Release"
+			optimize "on"
